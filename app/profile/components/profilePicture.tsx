38abfbd4ccb574582777818f2607a9712f096743
Vadym Musiienko <musiienkovadym@gmail.com>
2025-02-27 15:39:12 -0800

    profile page layout

---
+++ app/profile/components/profilePicture.tsx
@@ -0,0 +1,8 @@
+//TODO: Implement ProfilePicture component
+export default function ProfilePicture() {
+    return (
+        <div>
+            <h1>ProfilePicture</h1>
+        </div>
+    );
+}
---
+++ app/profile/page.tsx
@@ -0,0 +1,8 @@
+//TODO: Create ProfilePage here
+export default function ProfilePage() {
+    return (
+        <div>
+            <h1>ProfilePage</h1>
+        </div>
+    );
+}
