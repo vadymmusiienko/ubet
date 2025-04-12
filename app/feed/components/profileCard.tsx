7a41b28b8af6710b4a2941c34cd778f751ebda21
Ryo Kasai <ryo.kasai19@gmail.com>
2025-04-11 17:51:23 -0700

    I made some updates on profileCard.tsx please check if it's ok

--- app/feed/components/profileCard.tsx
+++ app/feed/components/profileCard.tsx
@@ -3,14 +3,35 @@
 //<img className= "rounded-lg" src="https://picsum.photos/400/400" alt="Your Image Description"></img> this worked!
 //<img className= "rounded-full w-20 h-20 mx-auto" src="/images/will.jpg" alt="Will img"></img> didn't work.
 
+import { prisma } from "@/lib/prisma";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
 
 
-export default function ProfileCard() {
+export default async function ProfileCard() {
+    
+    const { getUser } = getKindeServerSession();
+        const sessionUser = await getUser();
 
+        if (!sessionUser?.id) {
+            return (
+            <div className="text-center text-red-600 mt-10">
+                You must be logged in to view your profile.
+            </div>
+            );
+        }
+        
+
+    const user = await prisma.user.findUnique({
+        where: { id: sessionUser.id },
+        });
+
+
+
+    
     return (
             <div className="w-64 h-84 rounded-xl overflow-hidden shadow-lg bg-white text-center ml-10">
-                <h1 className="font-bold text-2xl text-center mb-2 mt-5"> William Haspel </h1>
+                <h1 className="font-bold text-2xl text-center mb-2 mt-5"> {user?.firstName} {user?.lastName} </h1>
                 <img className= "rounded-full w-20 h-20 mx-auto" src="https://picsum.photos/400/400" alt="Your Image Description"></img>
                 <p className='text-gray-700 ml-1 mr-1 mb-2'> Just trying my best <br></br>
                     out here </p>
@@ -24,3 +45,4 @@ export default function ProfileCard() {
         
     );
 }
+
